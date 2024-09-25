1530ed0af8f2c6ecff77bf5ddfc301663c5f5de8
Rory-CD <s223316766@deakin.edu.au>
2024-09-25 20:45:42 +1000

    Add git ignore

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === "production" ? "/6-2HD-website/" : "/",
+    publicPath: process.env.NODE_ENV === "production" ? "/vue-jenkins/" : "/",
 };
